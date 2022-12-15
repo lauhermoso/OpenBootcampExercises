8dbf3694e1b27f54fdca8e28348fbffc41c38b45
Lau <106471544+lauhermoso@users.noreply.github.com>
2022-12-15 18:16:01 +0100

    Javascript exercise 1

---
+++ JavaScript/exercise-1/index.js
@@ -0,0 +1,11 @@
+// Este es el mensaje de bienvenida
+console.log("Esta es la puerta de entrada al proyecto");
+
+/*
+Así se pone un comentario
+que afecte a varias líneas
+*/
+// Así un comentario que afecte a una sola línea
+
+// Este va a ser el mensaje que se envía para despedir
+console.log("Adiós");
